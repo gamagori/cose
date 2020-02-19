e7c9f90ebfc08e07e07414987424f0eeaf0b2245
grammar <grant.mattke.gordon@gmail.com>
2020-02-18 21:47:05 -0500

    some basics

---
+++ cose.sh
@@ -0,0 +1,24 @@
+# Install some tools that I like to have around.
+sudo apt-get install -y curl wget vim
+
+# Remove the side bar - I don't really use it.
+sudo apt-get remove gnome-shell-extension-ubuntu-dock
+
+# TODO: Install a nice font.
+# TODO: Adjust the color settings for the terminal.
+# TODO: Disable the scrollbar for the terminal.
+
+# TODO: Adjust the color settings for vim.
+# TODO: Install a nice IDE.
+
+# TODO: Install GNOME Shell Integration, so we can hide the top bar. 
+sudo apt-get install chrome-gnome-shell
+# Then, you can install it from here:
+# https://extensions.gnome.org/extension/545/hide-top-bar/
+
+# Other extensions I like:
+# https://extensions.gnome.org/extension/1128/hide-activities-button/
+# https://extensions.gnome.org/extension/1128/hide-activities-button/
+
+# Set a nice background. 
+curl -o ~/Pictures/nice_background.png https://i.imgur.com/sRpDOp3.jpg
